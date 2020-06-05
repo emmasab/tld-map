728de216c9a39d530dbcb1a145dd9f16ded889f0
Emma Sab <emmasab@users.noreply.github.com>
2020-06-05 15:34:34 -0700

    Fix map tiles not loading when index.html not specified

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ currentMap = currentMap.substr(0, currentMap.indexOf('.'));
 if (debug)
     currentMap += '_dev';
 
-if (currentMap == 'index')
+if (currentMap == 'index' || currentMap == '')
     currentMap = 'mountain_town';
 
 var bounds = [[85.02070774312594, -179.64843750000003], [-84.9747260970686, 179.12109375000003]];
